822f420132bac6df934f982b24c60075d0cb9b00
Miguel <dasilvamigueleduardo@gmail.com>
2024-08-01 20:12:15 -0300

    finalizando a base do projeto

--- local.py
+++ local.py
@@ -1,6 +1,23 @@
-from geopy.geocoders import google
+from geopy.geocoders import Nominatim  # Vou utilizar um metodo do geopy para me retornar a localização
 
-class local:
+geolocator = Nominatim(user_agent="my_app")
+
+
+class Local:
+    # O construtor recebe os paremetros x(latitude) e y(longetude)
+    def __init__(self, latitude:float, longitude:float):
+            self.latitude   = latitude 
+            self.longitude  = longitude
     
-    def __init__(self) -> None:
-        pass
+    
+    def __str__(self) -> str:
+         return f'{self.latitude}, {self.longitude}'
+            
+    # Esta função sera usada para retornar a localização do banco de dados
+    def pesquisa(self):
+        
+        try:
+            location = geolocator.reverse(f"{self.latitude}, {self.longitude}")
+            return location.address
+        except:
+            return None
---
+++ usaLoc.py
@@ -0,0 +1,18 @@
+from Local import Local
+
+# A entrada é definida aqui, 
+# 1 primeiro defino o input que recebe a cordenada
+# 2 em seguida separo a (str) por virgula
+# 3 garanto que (entrada) sera (float) com o metodo 'map' em seguida uso 'list' para não me retornar a cordenada do obj
+entrada = list(map(float, input('digite aqui o valor da coordenada separada por virgula: ').split(',')))
+coordenada = []
+
+# Filtro a lista para garantir apenas cordenada (x) e (y)
+for x in range(2):
+    coordenada.append(entrada[x])
+
+# crio uma instancio e insiro a cordenada no objeto
+local1 = Local(coordenada[0], coordenada[1])
+
+print(local1)
+print(local1.pesquisa())
